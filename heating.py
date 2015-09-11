25c34e2ee8b3ed8658e298ae50e93942f1461fc3
Steven Davies <github@steev.me.uk>
2015-09-11 20:07:53 +0100

    Split out HTTP server to separate file

--- heating.py
+++ heating.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
-import datetime, sys, threading, httplib2, os, syslog, time, inspect, pytz, urlparse, logging, logging.handlers, argparse
+import datetime, sys, threading, os, syslog, time, inspect, pytz, urlparse, logging, logging.handlers, argparse
 from sensortag import SensorTag
 from relay import Relay
+from httpserver import *
 
 from dateutil import parser
 from apiclient import discovery
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from SocketServer import ThreadingMixIn
 
 import oauth2client
 from oauth2client import client
@@ -223,39 +222,6 @@ class Heating(object):
       credentials = tools.run_flow(flow, store, flags)
       logger.info('Storing credentials to ' + credential_path)
     return credentials
-
-class HttpHandler(BaseHTTPRequestHandler):
-  heating = None
-
-  def do_GET(self):
-    parsed_path = urlparse.urlparse(self.path)
-    if parsed_path.path == '/current_temp':
-      logger.debug('Web request for /current_temp')
-      self.send_response(200)
-      self.end_headers()
-      self.wfile.write(str(self.heating.current_temp) + '\n')
-    elif parsed_path.path == '/desired_temp':
-      logger.debug('Web request for /desired_temp')
-      self.send_response(200)
-      self.end_headers()
-      if not self.next_event:
-        self.wfile.write(str(MINIMUM_TEMP))
-      else:
-        self.wfile.write(str(self.next_event[2]))
-    elif parsed_path.path == '/proportion':
-      logger.debug('Web request for /proportion')
-      self.send_response(200)
-      self.end_headers()
-      self.wfile.write(str(self.heating.proportional_time) + '\n')
-    else:
-      self.send_error(404)
-    return
-
-  def log_message(self, format, *args):
-    return
-
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-  '''Handle requests in a separate thread.'''
 
 class NoTemperatureException(Exception):
   pass
