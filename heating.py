dc02a05cd6deecc15832cc51a888da6a150c9176
Steven Davies <github@steev.me.uk>
2015-09-11 20:05:00 +0100

    Tidy up formatting, add desired_temp to web server

--- heating.py
+++ heating.py
@@ -143,18 +143,18 @@ class Heating(object):
 
     elif not self.next_event:
       #If we don't have another event, return.
-      logger.debug("No next event available.")
+      logger.debug('No next event available.')
       self.off(0)
 
     elif ((self.next_event[0] - current_time).seconds) + ((self.next_event[0] - current_time).days*24*60*60) > 60*60*3:
       #If next event begins more than three hours in the future, ignore it.
-      logger.debug("Next event is more than three hours in the future: %s" % (str(self.next_event[0])))
+      logger.debug('Next event is more than three hours in the future: %s' % (str(self.next_event[0])))
       self.off(0)
 
     elif self.current_temp > self.next_event[2]:
       #Ok, so there's a possibility we might need to turn on.
       #Is the current temperature higher than the next desired temperature?
-      logger.debug("Current temperature " + str(self.current_temp) + " is higher than the desired temperature " + str(self.next_event[2]))
+      logger.debug('Current temperature ' + str(self.current_temp) + ' is higher than the desired temperature ' + str(self.next_event[2]))
       self.off(0)
 
     else:
@@ -166,32 +166,32 @@ class Heating(object):
         new_proportional_time = 10
       elif new_proportional_time > PROPORTIONAL_HEATING_INTERVAL:
         new_proportional_time = PROPORTIONAL_HEATING_INTERVAL
-      logger.debug("New proportional time: " + str(new_proportional_time) + " minutes out of " + str(PROPORTIONAL_HEATING_INTERVAL))
+      logger.debug('New proportional time: ' + str(new_proportional_time) + ' minutes out of ' + str(PROPORTIONAL_HEATING_INTERVAL))
 
       #Start half an hour earlier for each degree the heating is below the desired temp
       if ((self.next_event[0] - current_time).seconds) + ((self.next_event[0] - current_time).days*24*60*60) < 60*60*3 \
            and ((self.next_event[0] - current_time).seconds) + ((self.next_event[0] - current_time).days*24*60*60) > 0:
         time_due_on = self.next_event[0] - datetime.timedelta(0,temp_diff * 30 * 60)
-        logger.debug("Initial warm-up, temp difference is " + str(temp_diff) + "; next event is at " + str(time_due_on))
+        logger.debug('Initial warm-up, temp difference is ' + str(temp_diff) + '; next event is at ' + str(time_due_on))
         if time_due_on < current_time:
           self.on(new_propotional_time)
       else:
         if self.proportional_time == 0:
-          logger.debug("Heating is off, turning on")
+          logger.debug('Heating is off, turning on')
           self.on(new_proportional_time)
         else:
-          logger.debug("Heating is proportional, shorten or lengthen the time interval as required")
+          logger.debug('Heating is proportional, shorten or lengthen the time interval as required')
           #Are we currently on or off?
           if self.relay.status == 0: #Off
             time_due_on = self.time_off + datetime.timedelta(0,(PROPORTIONAL_HEATING_INTERVAL * 60) - (self.proportional_time * 60))
             new_time_due_on = self.time_off + datetime.timedelta(0,(PROPORTIONAL_HEATING_INTERVAL * 60) - (new_proportional_time * 60))
-            logger.debug("Heating was off, due on at " + str(time_due_on) +". Now due on at " + str(new_time_due_on))
+            logger.debug('Heating was off, due on at ' + str(time_due_on) +'. Now due on at ' + str(new_time_due_on))
             if new_time_due_on < current_time:
               self.on(new_proportional_time)
           else: #On  
             time_due_off = self.time_on + datetime.timedelta(0,self.proportional_time * 60)
             new_time_due_off = self.time_on + datetime.timedelta(0,new_proportional_time * 60)
-            logger.debug("Heating was on, due off at " + str(time_due_off) +". Now due off at " + str(new_time_due_off))
+            logger.debug('Heating was on, due off at ' + str(time_due_off) +'. Now due off at ' + str(new_time_due_off))
             if new_time_due_off < current_time:
               self.off(new_proportional_time)
 
@@ -199,30 +199,29 @@ class Heating(object):
     self.processing_lock.release()     
 
   def get_credentials(self):
-    """Gets valid user credentials from storage.
+    '''Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
     the OAuth2 flow is completed to obtain the new credentials.
 
     Returns:
         Credentials, the obtained credential.
-    """
+    '''
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-heating.json')
+      os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'calendar-heating.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
     parser = argparse.ArgumentParser(parents=[tools.argparser])
     flags = parser.parse_args()
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', 'https://www.googleapis.com/auth/calendar.readonly')
-        flow.user_agent = 'Heating'
-        credentials = tools.run_flow(flow, store, flags)
-        logger.info('Storing credentials to ' + credential_path)
+      flow = client.flow_from_clientsecrets('client_secret.json', 'https://www.googleapis.com/auth/calendar.readonly')
+      flow.user_agent = 'Heating'
+      credentials = tools.run_flow(flow, store, flags)
+      logger.info('Storing credentials to ' + credential_path)
     return credentials
 
 class HttpHandler(BaseHTTPRequestHandler):
@@ -230,16 +229,24 @@ class HttpHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     parsed_path = urlparse.urlparse(self.path)
-    if parsed_path.path == "/temp":
-      logger.debug("Web request for /temp, sending response " + str(self.heating.current_temp))
+    if parsed_path.path == '/current_temp':
+      logger.debug('Web request for /current_temp')
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(str(self.heating.current_temp) + "\n")
-    elif parsed_path.path == "/proportion":
-      logger.debug("Web request for /proportion, sending response " + str(self.heating.proportional_time))
+      self.wfile.write(str(self.heating.current_temp) + '\n')
+    elif parsed_path.path == '/desired_temp':
+      logger.debug('Web request for /desired_temp')
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(str(self.heating.proportional_time) + "\n")
+      if not self.next_event:
+        self.wfile.write(str(MINIMUM_TEMP))
+      else:
+        self.wfile.write(str(self.next_event[2]))
+    elif parsed_path.path == '/proportion':
+      logger.debug('Web request for /proportion')
+      self.send_response(200)
+      self.end_headers()
+      self.wfile.write(str(self.heating.proportional_time) + '\n')
     else:
       self.send_error(404)
     return
@@ -248,7 +255,7 @@ class HttpHandler(BaseHTTPRequestHandler):
     return
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-  """Handle requests in a separate thread."""
+  '''Handle requests in a separate thread.'''
 
 class NoTemperatureException(Exception):
   pass
