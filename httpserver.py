533fdf10811f06e0f1907a8f3d70ddcb07827f74
Steven Davies <github@steev.me.uk>
2015-09-13 18:55:37 +0100

    Report temperatures for multiple devices

--- httpserver.py
+++ httpserver.py
@@ -10,27 +10,34 @@ class HttpHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     parsed_path = urlparse.urlparse(self.path)
-    if parsed_path.path == '/current_temp':
-      logger.debug('Web request for /current_temp, sending ' + str(self.heating.current_temp))
+    if '/current_temp' in parsed_path.path:
+      address = parsed_path.path[parsed_path.path.rfind('/') + 1:]
+      if address in self.heating.temp_sensors:
+        response = str(self.heating.temp_sensors[address].amb_temp) + '\n'
+      else:
+        response = ''
+        for mac, sensor in self.heating.temp_sensors.iteritems():
+          response += mac + '=' + str(self.heating.temp_sensors[mac].amb_temp) + '\n'
+      logger.info('Web request for ' + parsed_path.path + ', sending ' + response.replace('\n','\\n '))
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(str(self.heating.current_temp) + '\n')
+      self.wfile.write(response)
     elif parsed_path.path == '/desired_temp':
       if not self.heating.next_event or self.heating.next_event[0] > pytz.utc.localize(datetime.datetime.utcnow()):
         desired_temp = str(self.heating.minimum_temp)
       else:
         desired_temp = str(self.heating.next_event[2])
-      logger.debug('Web request for /desired_temp, sending ' + desired_temp)
+      logger.info('Web request for /desired_temp, sending ' + desired_temp)
       self.send_response(200)
       self.end_headers()
       self.wfile.write(desired_temp + '\n')
     elif parsed_path.path == '/proportion':
-      logger.debug('Web request for /proportion, sending ' + str(self.heating.proportional_time))
+      logger.info('Web request for /proportion, sending ' + str(self.heating.proportional_time))
       self.send_response(200)
       self.end_headers()
       self.wfile.write(str(self.heating.proportional_time) + '\n')
     else:
-      logger.debug('Web request for ' + parsed_path.path + ', ignoring')
+      logger.info('Web request for ' + parsed_path.path + ', ignoring')
       self.send_error(404)
     return
 
