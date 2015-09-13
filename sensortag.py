337257f5521b9abd4a565e6efef80de30333982e
Steven Davies <github@steev.me.uk>
2015-09-13 18:55:12 +0100

    Allow for multiple devices

--- sensortag.py
+++ sensortag.py
@@ -1,11 +1,16 @@
 import pygatt, struct, time, logging
 
+from pygatt.exceptions import NotConnectedError
+
 logger = logging.getLogger('heating')
 
 class SensorTag(object):
   def __init__(self, dongle, mac):
     self.mac = mac
     self.dongle = dongle
+    self.failures = 0
+    self.sent_alert = False
+    self.amb_temp = 30
     self.connect()
 
   def connect(self):
@@ -34,25 +39,33 @@ class SensorTag(object):
       #Turn red LED off
       self.tag.char_write_handle(0x4e, bytearray([0x00]))
       self.tag.char_write_handle(0x50, bytearray([0x00]))
-    except pygatt.exceptions.NotConnectedError as e:
-      self.connect()
-      raise NoTemperatureException('Unable to read temperature')
+    except pygatt.exceptions.NotConnectedError as nce1:
+      try:
+        self.connect()
+      except NotConnectedError as nce2:
+        self.failures += 1
+      raise NoTemperatureException('Unable to read temperature from ' + self.mac)
 
     (rawVobj, rawTamb) = struct.unpack('<hh', result)
     tAmb = rawTamb / 128.0
-    logger.debug('Got temperature ' + str(tAmb))
-    return tAmb
+    logger.info('Got temperature ' + str(tAmb) + ' from ' + self.mac)
+    self.amb_temp = tAmb
+    return self.amb_temp
 
   @staticmethod
-  def find_sensortag():
+  def find_sensortags():
     dongle = pygatt.backends.GATTToolBackend()
     logger.debug('Scanning for SensorTags')
     devices = dongle.scan()
+    sensortags = {}
     for device in devices:
       if device['name'] == 'CC2650':
         logger.info('Found SensorTag with address: ' + device['address'])
-        return SensorTag(dongle, device['address'])
-    raise Exception('No SensorTags found!')
+        sensortags[device['address']] = SensorTag(pygatt.backends.GATTToolBackend(), device['address'])
+    if len(sensortags) == 0:
+      raise Exception('No SensorTags found!')
+    dongle.stop()
+    return sensortags
 
 class NoTemperatureException(Exception):
   pass
